460c4ab5ef8b076ee9aae63491720d0131934dac
BarisJomarie <baris.j.bsinfotech@gmail.com>
2026-06-13 17:01:22 +0800

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ function App() {
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route index element={<Homepage />} />
-        <Route path="*" element={<NotFound />} />
         <Route path='/e-book' element={<EBookView/>}/>
+
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
